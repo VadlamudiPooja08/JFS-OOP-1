3ce65370bc026009e36fab97893a8e52bf208211
Vadlamudi Pooja <poojavadlamudipooja@gmail.com>
2025-07-09 19:46:01 +0530

    oopexampleDay-1

---
+++ Box.java
@@ -0,0 +1,13 @@
+package com.JFS.oopsconcepts;
+
+public class Box {
+	double width;
+	double height;
+	double depth;
+	public void displayBoxDetails() {
+		System.out.println("The box Width is:"+width);
+		System.out.println("The box Height is:"+height);
+		System.out.println("The box depth is:"+depth);
+	}
+
+}
