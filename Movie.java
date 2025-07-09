0129037c730b8d2ad3b0bb42e8bb70de4baeed13
Vadlamudi Pooja <poojavadlamudipooja@gmail.com>
2025-07-09 22:16:31 +0530

    OopDay-1

---
+++ Movie.java
@@ -0,0 +1,14 @@
+package com.JFS.oopsconcepts;
+
+public class Movie {
+	String movie;
+	String director;
+	double rating;
+	public void displayMovieDetails() {
+		System.out.println("Movie :"+movie);
+		System.out.println("Director :"+director);
+		System.out.println("Rating :"+rating);
+		
+		 }
+
+}
